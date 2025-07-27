066148c8c2dd39f49b64845d8b9810c1094494fa
Jesus Diaz Lara <jesus.diaz@elektra.com.mx>
2025-07-27 17:19:54 -0600

    idiazjs - demo

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Cargar el header
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+            // Reasignar el evento del menú después de cargar el header
+            document.getElementById('menu-toggle').addEventListener('click', () => {
+                const mobileMenu = document.getElementById('mobile-menu');
+                mobileMenu.classList.toggle('hidden');
+            });
+        })
+        .catch(error => console.error('Error al cargar el header:', error));
+});
